a6e01688fcbe4639ae9a91fcd5735665ab7fec4e
vibha1992 <vibha@dolphinwebsolution.com>
2020-06-30 20:10:34 +0530

    override module for Fixing bugs -- changes done by Gunjan

---
+++ app/code/Dolphin/Contact/Controller/Magento/Contact/Index/Post.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Dolphin\Contact\Controller\Magento\Contact\Index;
+
+class Post extends \Magento\Contact\Controller\Index\Post {
+
+	public function execute() {
+		return parent::execute();
+	}
+
+}
---
+++ app/code/Dolphin/Wishlist/Block/Magento/Wishlist/Customer/Wishlist.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Dolphin\Wishlist\Block\Magento\Wishlist\Customer;
+
+class Wishlist extends \Magento\Wishlist\Block\Customer\Wishlist {
+
+/**
+ * Preparing global layout
+ *
+ * @return $this
+ */
+	protected function _prepareLayout() {
+
+		return $this;
+	}
+
+}
