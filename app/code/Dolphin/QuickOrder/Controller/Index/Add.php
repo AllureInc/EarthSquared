aa54d12c1b363633cc28ce20b6b240717e0cd8cd
vibha-1 <vibha@dolphinwebsolution.com>
2020-07-31 19:34:46 +0530

    working on quickorder

--- app/code/Dolphin/QuickOrder/Controller/Index/Add.php
+++ app/code/Dolphin/QuickOrder/Controller/Index/Add.php
@@ -3,19 +3,25 @@ namespace Dolphin\QuickOrder\Controller\Index;
 
 class Add extends \Magento\Framework\App\Action\Action
 {
-	protected $_pageFactory;
+    protected $_pageFactory;
+    protected $coreSession;
 
-	public function __construct(
-		\Magento\Framework\App\Action\Context $context,
-		\Magento\Framework\View\Result\PageFactory $pageFactory)
-	{
-		$this->_pageFactory = $pageFactory;
-		return parent::__construct($context);
-	}
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Framework\Session\SessionManagerInterface $coreSession,
+        \Magento\Framework\View\Result\PageFactory $pageFactory) {
+        $this->_pageFactory = $pageFactory;
+        $this->coreSession = $coreSession;
 
-	public function execute()
-	{
-		echo $_REQUEST['qty'];
-		exit;
-	}
+        return parent::__construct($context);
+    }
+
+    public function execute()
+    {   
+        $items = $_REQUEST['rowcollection'];                        
+        $this->coreSession->setQuickData($items);        
+        $sessionData =  $this->coreSession->getQuickData();    
+        echo json_encode($sessionData);        
+        exit;
+    }
 }
